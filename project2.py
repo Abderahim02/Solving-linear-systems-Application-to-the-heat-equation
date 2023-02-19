6fc77b23d7e642c385595a9f0ef0a73c044e80a6
Abderahim Lagraoui <abderahim.lagraoui@bordeaux-inp.fr>
2023-02-19 14:57:53 +0100

    cholesky

--- project2.py
+++ project2.py
@@ -1,27 +1,42 @@
 import numpy as np 
 
 def cholesky(A):
-    n=A.shape[0]
-    T=np.zeros((n,n))
-    for i in range(0, n ,1):
-        s=0
-        for k in range (0, i): ##remplir la diagonale 
-            s+=T[i][k]
-        T[i][i] = np.sqrt(A[i][i] - s)
-        for j in range (i, n): ## remplir la partie inferieure
-            t=0
-            for k in range (0, i):
-                t+=T[i][k] * T[j][k]
-            T[j][i]=(A[i][j] - t)/A[i][i]
-    return T
-
+    n = len(A)
+    L = np.zeros((n,n))
+    for i in range(n):
+        for k in range(i+1):
+            tmp_sum = sum(L[i][j] * L[k][j] for j in range(k))
+            if (i == k):
+                L[i][k] = np.sqrt(A[i][i] - tmp_sum)
+            else:
+                L[i][k] = (1 / L[k][k] ) * (A[i][k] - tmp_sum)
+    return L
 
 def test_chloesky():
     A=np.array([[1,1,1,1],
                 [1,5,5,5],
                 [1,5,14,14],
                 [1,5,14,15]])
+
+    B=np.array([[1,1,1,1],
+                [1,2,2,2],
+                [1,2,3,3],
+                [1,2,3,4]])
     print(cholesky(A))
+    print("\n")
+    print(cholesky(B))
 
 test_chloesky()
     
+def cholesky_incomplete(A):
+    n = len(A)
+    L = np.zeros((n,n))
+    for i in range(n):
+        for k in range(i+1):
+            if ( A[i][k] != 0):
+                tmp_sum = sum(L[i][j] * L[k][j] for j in range(k))
+                if (i == k):
+                    L[i][k] = np.sqrt(A[i][i] - tmp_sum)
+                else:
+                    L[i][k] = (1 / L[k][k] ) * (A[i][k] - tmp_sum)
+    return L
